eaef3e1defa0c5f9379a3dd6124e8912a09c5aed
kenyansa <kenyansajustine@gmail.com>
2022-03-04 14:51:45 +0300

    added background color

---
+++ ugali.js
@@ -0,0 +1,9 @@
+//declaring an empty hashmap
+let narobiBedroom = new Map();
+//adding values o a hashmap. the firts item is a key and the second is a value
+narobiBedroom.set("Sunday","Hacking")
+narobiBedroom.set("Monday", "Upuzi")
+//get value by key
+x=narobiBedroom.get ("Monday")
+
+console.log(x)
